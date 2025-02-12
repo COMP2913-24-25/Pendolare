8e210f406f9a669880be78c37e3266d2b558e358
Alexander McCall <146675021+alexmc-c@users.noreply.github.com>
2025-02-12 18:15:28 +0000

    Added testing Docker image, updated run command to include test and deploy

--- Pendo.PaymentService/runPaymentService.sh
+++ Pendo.PaymentService/runPaymentService.sh
@@ -1,18 +1,38 @@
-echo "|| Running Payment Service... ||"
+# 
+# SHELL Script to deploy Pendo.PaymentService locally in a Docker Container
+# Author: Alexander McCall
+# Created: 12/02/2025
+#
+
+printf "\033[1m============================= Running Payment Service... ==============================\033[1m\n"
 
 # remove any existing contains that are already running / exist using that image
-echo "Removing Docker Containers using old image..."
+printf "\033[1m============================= Removing Old Containers... ==============================\033[1m\n"
+
 docker stop Pendo.PaymentService
 docker rm -f Pendo.PaymentService
-echo "Completed"
 
-# build docker image
-echo "Building Docker Image..."
-docker build -t pendo.paymentservice.image .
-echo "Image built"
+docker stop Pendo.PaymentService.Tests
+docker rm -f Pendo.PaymentService.Tests
 
-# run new docker container with updated image
-echo "Creating new Docker container..."
-docker run -d -p 5004:5004 --name Pendo.PaymentService pendo.paymentservice.image
-echo "Container created"
-echo "Pendo.PaymentService is running on localhost:5004"
+printf "\e[32mCompleted\e[0m\n"
+
+# build fastapi image
+printf "\033[1m============================= Building Docker Deploy Image ============================\033[1m\n"
+docker build -f Dockerfile.deploy -t pendo.paymentservice.deploy.image .
+printf "\033[1m Completed Deploy image \033[1m\n"
+
+# build pytest image
+printf "\033[1m============================= Building Docker Test Image ==============================\033[1m\n"
+docker build -f Dockerfile.tests -t pendo.paymentservice.test.image .
+printf "\033[1m Completed Tests image \033[1m\n"
+
+# run automated tests
+printf "\033[1m============================= Running Pytest ==========================================\033[1m\n"
+docker run --name Pendo.PaymentService.Tests pendo.paymentservice.tests.image
+
+# run service
+printf "\033[1m============================= Running Pendo.PaymentService.Deploy =====================\033[1m\n"
+docker run -d -p 5004:5004 --name Pendo.PaymentService pendo.paymentservice.deploy.image
+printf "\e[32mContainer Created\e[0m\n"
+printf "\e[32mPendo.PaymentService is running on localhost:5004\e[0m\n"
