b94e1471995be07e3d305328c433ba62f89dbc2d
sc23c2w <146746557+sc23c2w@users.noreply.github.com>
2025-02-13 14:09:18 +0000

    Added Journery Folder Structures and Updated Journey Tables

--- Pendo.Database/Tables/journey/CommutingDetails.sql
+++ Pendo.Database/Tables/journey/CommutingDetails.sql
@@ -11,7 +11,7 @@ CREATE TABLE [journey].[CommutingDetails]
   [RepeatFrequency] NVARCHAR(100) NULL,
   [RepeatUntilDate] DATE NOT NULL,
   [DayOfWeek] NVARCHAR(100) NULL,
-  [Time] INT NOT NULL,
+  [Time] NVARCHAR(100) NULL,
 
   CONSTRAINT FK_CommutingDetails_JourneyId FOREIGN KEY ([JourneyId]) 
   REFERENCES [journey].[Journeys](JourneyId)
