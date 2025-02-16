01220b0f3f89791dd7f27d2bbeda23b4f67d65f7
sc232jm <145260982+sc232jm@users.noreply.github.com>
2025-02-16 13:27:09 +0000

    Updated layout

--- Pendo.ClientApp/app/home/_layout.tsx
+++ Pendo.ClientApp/app/home/_layout.tsx
@@ -1,11 +1,21 @@
 import { Stack } from "expo-router";
 
-const Layout = () => {
+export default function HomeLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="tabs" options={{ headerShown: false }} />
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: 'slide_from_right',
+      }}
+    >
+      <Stack.Screen name="tabs" />
+      <Stack.Screen 
+        name="chat/[id]" 
+        options={{
+          presentation: 'card',
+          animation: 'slide_from_right',
+        }}
+      />
     </Stack>
   );
-};
-
-export default Layout;
+}
