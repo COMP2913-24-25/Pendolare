0207a9955ab776acba0a73e29f9183f4374170a0
Alexander McCall <146675021+alexmc-c@users.noreply.github.com>
2025-02-25 00:48:34 +0000

    Sketched out payment endpoints

--- Pendo.PaymentService/app/src/payment_service.py
+++ Pendo.PaymentService/app/src/payment_service.py
@@ -5,15 +5,94 @@
 #
 
 from fastapi import FastAPI
+from src.PendoDatabase import Transaction
+from pydantic import BaseModel
+
+class Take
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
+@app.post("/takeStripe")
+def take():
+
+    # TODO: Complete TakeStipe endpoint
+
+    # get fee - from journey?
+    # get adminID
+
+    # increase pending balance by journey value (minus fee!)
+    # increase fee balance
+
     return {"Hello" : "World"}
 
-@app.get("/take")
+@app.post("/takeBalance")
 def take():
+
+    # TODO: Complete TakeStipe endpoint
+
+    # get fee - from journey?
+    # get adminID
+
+    # increase pending balance by journey value (minus fee!)
+    # increase fee balance
+
+    return {"Hello" : "World"}
+
+@app.post("/confirm")
+def confirm():
+
+    # TODO: Complete Confirm endpoint
+
+    # input: bookingID or journeyID, advertiserID
+
+    # get fee - from journey?
+
+    # decrease pending balance by journey value (minus fee!)
+    # increase non-pending balance by journey value (minus fee!)
+
+
+    return {"Hello" : "World"}
+
+@app.get("/view")
+def view():
+
+    # TODO: Complete View endpoint
+    
+    # input: userID
+
+    # Query non-pending balance
+
+    # Query pending balance
+
+    # return both
+
+    return {"status" : "success",
+            "pending": "value",
+            "non-pending": "value"}
+
+@app.get("/calculate")
+def calculate():
+
+    # TODO: Complete calculate endpoint
+
+    # input: userID, journeyID, useCredit?
+
+    # query non-pending balance
+
+    # return journey cost - credit if possible and requested
+
+@app.post("/refund")
+def refund():
+
+    # TODO: Complete refund endpoint
+
+    return {"Hello" : "World"}
+
+@app.post("/payout")
+def payout():
+    
+    # TODO: Complete Payout endpoint
+
     return {"Hello" : "World"}
 
 def some_testing_function(param):
