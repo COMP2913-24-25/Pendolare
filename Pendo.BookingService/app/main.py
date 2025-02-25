18351d44611a82d47465d44c9fad32be959ce175
James Kinley <jameskinley24@gmail.com>
2025-02-25 13:41:34 +0000

    Created shell of API

--- Pendo.BookingService/app/main.py
+++ Pendo.BookingService/app/main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI, HTTPException, Depends
 from .db_provider import get_db, Session, text
 
-# FastAPI app
-app = FastAPI()
+app = FastAPI(
+    title="Pendo.BookingService.Api", 
+    version="1.0.0",
+    root_path="/api")
 
-@app.get("/healthcheck")
+@app.get("/HealthCheck", tags=["HealthCheck"])
 def test_db(db: Session = Depends(get_db)):
     try:
         db.execute(text("SELECT 1"))
         return {"db_connection": "successful"}
     except Exception as e:
         return {"db_connection": "failed", "error": str(e)}
+
+@app.post("/GetBookings", tags=["Get Bookings"])
+def get_bookings(db: Session = Depends(get_db)):
+    return {}
+
+@app.post("/CreateBooking", tags=["Create Bookings"])
+def create_booking(db: Session = Depends(get_db)):
+    return {}
+
+@app.post("/UpdateBooking", tags=["Update Bookings"])
+def update_booking(db: Session = Depends(get_db)):
+    return {}
