a356b2cead1b7cd373e3a10da6bc87a9c683dafa
James Kinley <jameskinley24@gmail.com>
2025-02-27 17:57:05 +0000

    wrote shell of dvla api, adding approve_booking_ammendmand command

---
+++ Pendo.BookingService/app/dvla_api.py
@@ -0,0 +1,33 @@
+import requests
+
+class VehicleEnquiryClient:
+    """
+    VehicleEnquiryClient is a client for the DVLA vehicle enquiry API.
+    """
+    def __init__(self, api_key, logger):
+        self.api_key = api_key
+        self.logger = logger
+        self.path = "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles"
+        self.headers = {
+            "x-api-key" : api_key,
+            "Content-Type" : "application/json"
+        }
+
+    def GetVehicleDetails(self, registration):
+        """
+        GetVehicleDetails method sends a request to the DVLA vehicle enquiry API to get vehicle details.
+        """
+        request = { "registrationNumber" : registration }
+        self.logger.debug(f"Vehicle enquiry request: {request}")
+
+        #response = requests.post(self.path, headers=self.headers, json=request)
+        response = { "car": "red one"}
+
+        self.logger.debug(f"Vehicle enquiry response: {response.text}")
+        return self._formatResponse(response)
+
+    def _formatResponse(self, response):
+        """
+        _formatResponse method formats the response from the DVLA vehicle enquiry API.
+        """
+        return response["car"]
