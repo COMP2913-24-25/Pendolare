d92df96b0eeace9b5fd2eb5a14865e05236cbd56
Alexander McCall <146675021+alexmc-c@users.noreply.github.com>
2025-03-04 00:54:37 +0000

    Fixed pytest docker issue

--- Pendo.PaymentService/app/src/db/PendoDatabaseProvider.py
+++ Pendo.PaymentService/app/src/db/PendoDatabaseProvider.py
@@ -8,7 +8,7 @@ env_path = find_dotenv('../configuration/.env')
 load_dotenv(env_path)
 environment = os.getenv("ENV", "Production")
 
-configPath = f"/code/configuration/appsettings.{environment}.json"
+configPath = f"/code/src/configuration/appsettings.{environment}.json"
 configProvider = ConfigurationProvider(configPath)
 
 engine = create_engine(configProvider.database.getDbUrl())
