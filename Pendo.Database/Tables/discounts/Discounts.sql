c8aac641cf72b48871967e928a9f726e739cfa26
Lara Glenn <146271102+lara-glenn@users.noreply.github.com>
2025-03-12 21:28:20 +0000

    Created table for discounts

--- Pendo.Database/Tables/discounts/Discounts.sql
+++ Pendo.Database/Tables/discounts/Discounts.sql
@@ -3,3 +3,13 @@ Author: Lara Glenn
 Created: 12/03/2025
 Description: Creates Discounts Table
 */
+
+CREATE TABLE [discounts].[Discounts]
+(
+    [DiscountID] UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
+    [WeeklyJourneys]  INT NOT NULL,
+    [DiscountPercentage] FLOAT NOT NULL,
+    [CreateData] DATETIME2 NOT NULL DEFAULT GETUCTDATE(),
+
+    CONSTRAINT CHECK_DiscountPercentage CHECK (DiscountPercentage >= 0 AND DiscountPercentage <= 1)
+);
