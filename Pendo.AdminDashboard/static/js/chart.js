2eabc1d1945169978c51bd1ada9074ac38b2df66
shayodonnell <115416152+shayodonnell@users.noreply.github.com>
2025-03-13 20:55:49 +0000

    Implemented get requests for chart data and labels

--- Pendo.AdminDashboard/static/js/chart.js
+++ Pendo.AdminDashboard/static/js/chart.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['1st week', '2nd week', '3rd week', '4th week', '5th week', '6th week'],
+            labels: window.chartLabels,
             datasets: [{
                 label: 'Revenue in £',
-                data: [4345, 1233, 7899, 4567, 2346, 6786],
+                data: window.chartData,
                 borderWidth: 1
             }]
         },
