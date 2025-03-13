91b0486b19895acab6ed315503b6173f3ae2d438
sc232jm <145260982+sc232jm@users.noreply.github.com>
2025-03-13 17:35:38 +0000

    Add project configuration files and update requirements; remove obsolete Dockerfiles and SQL scripts

--- Pendo.Database/Scripts/Create-Indexes.sql
+++ Pendo.Database/Scripts/Create-Indexes.sql
@@ -5,3 +5,22 @@ Description: Creates all additional indexes not defined in the table definitions
 */
 
 CREATE NONCLUSTERED INDEX IX_User_UserType ON [identity].[User]([UserTypeId]);
+
+GO;
+
+CREATE INDEX IX_OtpLogin_UserId ON [identity].[OtpLogin] (UserId)
+
+GO;
+
+-- Messaging Service Indexes
+CREATE INDEX IX_Messages_ConversationId ON [messaging].[Messages]([ConversationId]);
+
+GO;
+
+CREATE INDEX IX_Messages_SenderId ON [messaging].[Messages]([SenderId]);
+
+GO;
+
+CREATE INDEX IX_ConversationParticipants_UserId ON [messaging].[ConversationParticipants]([UserId]);
+
+GO;
