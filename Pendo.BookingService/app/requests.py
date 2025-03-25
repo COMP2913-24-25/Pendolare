0cf07944c0538ee258aab9b4530c684e815369e9
James Kinley <jameskinley24@gmail.com>
2025-03-25 15:07:52 +0000

    Changed booking service to be able to get bookings for a driver

--- Pendo.BookingService/app/requests.py
+++ Pendo.BookingService/app/requests.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 class GetBookingsRequest(BaseModel):
     UserId: UUID
+    DriverView: bool = False
 
 class AddBookingAmmendmentRequest(BaseModel):
     UserId: UUID
