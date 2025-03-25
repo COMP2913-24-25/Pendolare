e37cf16ed7c3ed960867312299cb239be36d8993
James Kinley <jameskinley24@gmail.com>
2025-03-25 15:14:11 +0000

    Added ability to get a user's journeys via journey service

--- Pendo.JourneyService/app/request_lib.py
+++ Pendo.JourneyService/app/request_lib.py
@@ -8,6 +8,7 @@ class GetJourneysRequest(BaseModel):
     BootHeight: Optional[float] = 0
     BootWidth: Optional[float] = 0
     StartDate: Optional[datetime.datetime] = None
+    EndDate: Optional[datetime.datetime] = None
     JourneyType: Optional[int] = 0
     MaxPrice: Optional[float] = 0
     NumPassengers: Optional[int] = 0
@@ -17,6 +18,7 @@ class GetJourneysRequest(BaseModel):
     EndLat: Optional[float] = 0
     EndLong: Optional[float] = 0
     SortByPrice: Optional[str] = None
+    DriverView: Optional[bool] = False
 
 class CreateJourneyRequest(BaseModel):
     UserId: UUID
