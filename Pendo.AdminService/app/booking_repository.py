b9e921fbfc3f9da261c64d336ecad17af0cfe1c7
shayodonnell <115416152+shayodonnell@users.noreply.github.com>
2025-03-26 17:34:26 +0000

    Removed comment

--- Pendo.AdminService/app/booking_repository.py
+++ Pendo.AdminService/app/booking_repository.py
@@ -17,5 +17,3 @@ class BookingRepository:
     def GetBookingsInWindow(self, start_date, end_date):
         return self.db_session.query(Booking)\
             .filter(Booking.RideTime >= start_date, Booking.RideTime <= end_date, Booking.BookingStatusId == 4).all()
-    
-    #etc etc, add more methods here
