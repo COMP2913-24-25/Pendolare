954322723adad4104145bd1cd21c39860eb1eb42
sc23c2w <146746557+sc23c2w@users.noreply.github.com>
2025-03-26 19:24:12 +0000

    Implemented CompletedBooking and RefundPayment Endpoints

--- Pendo.PaymentService/app/src/endpoints/CompletedBookingCmd.py
+++ Pendo.PaymentService/app/src/endpoints/CompletedBookingCmd.py
@@ -29,7 +29,66 @@ class CompletedBookingCommand:
             
             # TODO: Complete Confirm endpoint - Catherine
             
-            # input: bookingID
+            # input: CompletedBookingRequest
+            # schema:
+                # BookingId: UUID
+                # LatestPrice: float
+
+            PassengerId = self.PaymentRepository.GetBookingById(self.booking_id).UserId
+
+            DriverId = self.PaymentRepository.GetBookingById(self.booking_id).journey_.UserId
+
+            AdminFee = self.PaymentRepository.GetBookingById(self.booking_id).FeeMargin
+
+            self.PaymentRepository.UpdateNonPendingBalance(PassengerId, -1 * LatestPrice)
+
+            transaction = Transaction(
+                        UserId = PassengerId, 
+                        Value = float(LatestPrice),
+                        CurrencyCode = "gbp",
+                        TransactionStatusId = 1,
+                        TransactionTypeId = 3,
+                        CreateDate = datetime.datetime.now(),
+                        UpdateDate = datetime.datetime.now()
+                    )
+
+            self.PaymentRepository.CreateTransaction(transaction)
+
+            #admin fee?
+
+            self.PaymentRepository.UpdatePendingBalance(DriverId, -1 * LatestPrice)
+
+            transaction = Transaction(
+                        UserId = DriverId, 
+                        Value = float(LatestPrice),
+                        CurrencyCode = "gbp",
+                        TransactionStatusId = 1,
+                        TransactionTypeId = 1,
+                        CreateDate = datetime.datetime.now(),
+                        UpdateDate = datetime.datetime.now()
+                    )
+
+            self.PaymentRepository.CreateTransaction(transaction)
+
+            Margin = round(AdminFee * LatestPrice, 2)
+            Price = NewPrice - Margin
+
+            self.PaymentRepository.UpdateNonPendingBalance(DriverId, Price)
+
+            transaction = Transaction(
+                        UserId = DriverId, 
+                        Value = float(Price),
+                        CurrencyCode = "gbp",
+                        TransactionStatusId = 1,
+                        TransactionTypeId = 2,
+                        CreateDate = datetime.datetime.now(),
+                        UpdateDate = datetime.datetime.now()
+                    )
+
+            self.PaymentRepository.CreateTransaction(transaction)
+
+            
+
 
             # decrease booker non-pending by most recent booking value from booking ammendment
 
